76caeaa0c262642c69201ee923ba48f4dd362769
lampv <phamvanlam1991@gmail.com>
2019-06-06 11:56:01 +0700

    first add

---
+++ src/main/java/mainProgram.java
@@ -0,0 +1,16 @@
+public class mainProgram {
+    public static void main(String[] args) {
+        Fan fan1= new Fan();
+        Fan fan2= new Fan();
+        fan1.setSpeed(Fan.FAST);
+        fan1.setRadius(10);
+        fan1.setColor("Yellow");
+        fan1.setOn(true);
+        fan2.setSpeed(Fan.MEDIUM);
+        fan2.setRadius(5);
+        fan2.setColor("Blue");
+        fan2.setOn(false);
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+}
